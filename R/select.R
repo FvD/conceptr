d97e591992ebf399c26c4ccdffebb2187ac11688
FvD <FvD@users.noreply.github.com>
2015-10-14 17:18:57 -0600

    Outline for select

--- R/select.R
+++ R/select.R
@@ -1,7 +1,11 @@
 #' Select at level
-#' 
+#'
 #' Get all the variables at the given level (level 1 for the highest level). If
 #' the requested levels do not exist give a warning
 select_level <- function(data, hierarchy, level) {
   print("The select_level function has not been implemented yet.")
+
+  # Get the names of the concepts at the requested level
+
+  # Roll up the data to the names at the requested level
 }
