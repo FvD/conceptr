8a9602c1a23743db620b6b7a0a85a684f9724941
FvD <FvD@users.noreply.github.com>
2015-10-29 17:21:53 -0600

    Fixed levels in reference_hierarchy

--- tests/testthat/test-visualize.R
+++ tests/testthat/test-visualize.R
@@ -8,17 +8,17 @@ context("Visualize")
 reference_hierarchy <- list(name="", children=list(
                           list(name="Level1-A", children=list(
                             list(name="Level2-AA", children=list(
-                              list(name="Level3-AAA"))),
-                          list(name="Level2-AB"),
+                              list(name="Level3-AAA")))),
+                            list(name="Level2-AB")),
                           list(name="Level1-B", children=list(
                             list(name="Level2-BA"),
                             list(name="Level2-BB"),
-                            list(name="Level2-BD")),
+                            list(name="Level2-BD"))),
                           list(name="Level1-C", children=list(
                             list(name="Level2-CA", children=list(
                               list(name="Level3-CAA"),
                               list(name="Level3-CAB"),
-                              list(name="Level4-CABA"))),
+                              list(name="Level4-CABA"))))),
                           list(name="Level1-D")
-                          )))))))
+                          ))
 
