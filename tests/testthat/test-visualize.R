fdef8db4b498d6e514043c30b3ddf962fc2971b2
FvD <FvD@users.noreply.github.com>
2015-10-30 14:40:53 -0600

    fixed reference hierarchy lists of lists
    there were still issues with the list definition for networkD3 to
    acccept it as correct.

--- tests/testthat/test-visualize.R
+++ tests/testthat/test-visualize.R
@@ -8,17 +8,36 @@ context("Visualize")
 reference_hierarchy <- list(name="", children=list(
                           list(name="Level1-A", children=list(
                             list(name="Level2-AA", children=list(
-                              list(name="Level3-AAA")))),
-                            list(name="Level2-AB")),
+                              list(name="Level3-AAA")
+                              )),
+                            list(name="Level2-AB")
+                          )),
                           list(name="Level1-B", children=list(
                             list(name="Level2-BA"),
                             list(name="Level2-BB"),
-                            list(name="Level2-BD"))),
+                            list(name="Level2-BD")
+                          )),
                           list(name="Level1-C", children=list(
                             list(name="Level2-CA", children=list(
                               list(name="Level3-CAA"),
                               list(name="Level3-CAB"),
-                              list(name="Level4-CABA"))))),
+                              list(name="Level4-CABA")
+                              ))
+                          )),
                           list(name="Level1-D")
-                          ))
+                        ))
+
+build2 <- list(name="", children=list(
+  list(name="Level1-A"),
+  list(name="Level1-B"),
+  list(name="Level1-C"),
+  list(name="Level1-D")
+))
+
+build <- list(name="", children=list(
+  list(name="Level1-A"),
+  list(name="Level1-B"),
+  list(name="Level1-C"),
+  list(name="Level1-D")
+  ))
 
